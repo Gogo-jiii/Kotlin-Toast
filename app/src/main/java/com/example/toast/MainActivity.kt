785a6f694bb873fb1ec4ce9d596d70b0ead3b8db
VgionTech <(0$t\U9i9b.{3k*>
2022-04-29 16:16:25 +0530

    init

---
+++ app/src/main/java/com/example/toast/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.toast
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "IT Wala...", Toast.LENGTH_LONG).show()
+    }
+}
